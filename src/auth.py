635f32b5858e31024a99941a3b33272d3b60bd5d
venkata-234 <venkatamanoj234@gmail.com>
2025-04-06 17:29:35 +0530

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -8,20 +8,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send']
 
 def authenticate_google():
     creds_base64 = os.getenv("GOOGLE_CREDENTIALS_JSON")
+    
+    if not creds_base64:
+        raise ValueError("Google credentials not found in environment variable 'GOOGLE_CREDENTIALS_JSON'")
+
     creds_json = base64.b64decode(creds_base64).decode("utf-8")
-    with open("credentials.json", "w") as creds_file:
-        creds_file.write(creds_json)
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    creds = flow.run_local_server(port=0)
+    creds_dict = json.loads(creds_json)
+
+    creds = None
+    if creds_dict:
+        flow = InstalledAppFlow.from_client_config(creds_dict, SCOPES)
+        creds = flow.run_local_server(port=0)
+
     service = build('gmail', 'v1', credentials=creds)
     return service
 
 def authenticate_slack():
     slack_token = os.getenv("SLACK_API_TOKEN")
+    if not slack_token:
+        raise ValueError("Slack API token not found in environment variable 'SLACK_API_TOKEN'")
+    
     client = WebClient(token=slack_token)
     return client
