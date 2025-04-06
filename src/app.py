614698d9100cec15c2adab295cbd597b2777e23d
venkata-234 <venkatamanoj234@gmail.com>
2025-04-06 21:59:43 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, jsonify
-from auth import authenticate_google, authenticate_slack
-from email_fetcher import fetch_emails
-from email_processor import generate_reply
-from email_sender import send_email
-from slack_integration import send_to_slack
-
+from .auth import authenticate_google, authenticate_slack  
+from .email_fetcher import fetch_emails 
+from .email_processor import generate_reply  
+from .email_sender import send_email 
+from .slack_integration import send_to_slack 
 app = Flask(__name__)
 google_service = authenticate_google()
 slack_client = authenticate_slack()
+
 @app.route('/process_and_reply', methods=['GET'])
 def process_and_reply():
     emails = fetch_emails(google_service)
@@ -20,6 +20,6 @@ def process_and_reply():
     send_to_slack(slack_client, "#general", reply)
 
     return jsonify({"message": "Replied to the email and sent a notification to Slack."})
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
-
